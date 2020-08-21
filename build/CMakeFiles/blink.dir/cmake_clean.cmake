c2926439cb4a1b9005a40491f940bd67a17d74c5
itsjustaplant <alperensahin442@gmail.com>
2020-08-21 11:35:58 +0300

    initial commit

---
+++ build/CMakeFiles/blink.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blink.dir/src/main.c.o"
+  "blink.elf"
+  "blink.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/blink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

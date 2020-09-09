9c220797b5ddab905302ef639c7b5f69066bc629
itsjustaplant <alperensahin442@gmail.com>
2020-09-09 11:47:09 +0300

    Unnecessary files are deleted

--- build/CMakeFiles/hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/strip.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/strip"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/strip.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
